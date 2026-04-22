628aa1c46d41bb541fa48e8075e8102cb9533fd4
Mikaella Souza <mikaella-souza@github.com>
2026-04-22 14:11:24 -0500

    Mostrar erro real na alteração de senha (debug)

--- src/components/ModalSenha.jsx
+++ src/components/ModalSenha.jsx
@@ -26,7 +26,7 @@ export default function ModalSenha({ usuario, onFechar, onSalvar }) {
     })
 
     if (error) {
-      setErro('Erro ao alterar senha. Tente novamente.')
+      setErro(`Erro: ${error.message}`)
     } else {
       onSalvar()
     }
